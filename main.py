7ab3d38ce8f66668338fb6459a18f1d71d242d6a
GSelvakumar <gselvaofficial@outlook.com>
2021-05-03 10:20:47 +0530

    Basic boilerplate added

---
+++ main.py
@@ -0,0 +1,19 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("India States Game")
+image = "India-state.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+
+
+
+#Code to plot in images(.gif format)
+# def get_mouse_click_corr(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(get_mouse_click_corr)
+# turtle.mainloop()
