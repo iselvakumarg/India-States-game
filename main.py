1a190d75d48d2080e26625ed7ca705750da51d2b
GSelvakumar <gselvaofficial@outlook.com>
2021-05-03 11:33:47 +0530

    Project completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas as pd
 
 screen = turtle.Screen()
 screen.title("India States Game")
@@ -6,11 +7,35 @@ image = "India-state.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+data = pd.read_csv("states_data.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
+while len(guessed_states) < 29:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/29 states correct",
+                                    prompt="What's another state's name ?").title()
+    print(answer_state)
 
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+        
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 
-
-#Code to plot in images(.gif format)
+screen.exitonclick()
+# Code to plot in images(.gif format)
 # def get_mouse_click_corr(x, y):
 #     print(x, y)
 #
